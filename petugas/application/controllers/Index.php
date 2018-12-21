abbca57a219a0b4d58e1bbd847b6830902b53031
Alfian Dimas <kidaliez@gmail.com>
2018-12-22 03:14:03 +0700

    update

---
+++ petugas/application/controllers/Index.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Error extends CI_Controller {
+
+	public function index()
+	{
+    echo 'asd';
+	}
+
+}
+
+/* End of file Error.php */
+/* Location: ./application/controllers/Error.php */
