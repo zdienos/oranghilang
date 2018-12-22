aa36d65cbf9ffa5d4f652dbe4d79ada7d6e440b6
Alfian Dimas <kidaliez@gmail.com>
2018-12-22 09:22:20 +0700

    delete

--- petugas/application/config/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-$route['default_controller'] = 'index';
-$route['404_override'] = 'error';
-$route['translate_uri_dashes'] = FALSE;
