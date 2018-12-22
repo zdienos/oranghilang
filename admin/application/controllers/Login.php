253171ecd613cb485017e01df0829a2f9e813a56
Alfian Dimas <kidaliez@gmail.com>
2018-12-22 18:09:31 +0700

    update validate

--- admin/application/controllers/Login.php
+++ admin/application/controllers/Login.php
@@ -21,38 +21,41 @@ class Login extends CI_Controller {
 
 	public function validate()
 	{		 
+    if ($this->input->server('REQUEST_METHOD') == 'POST'){
+	    $this->form_validation->set_rules($this->user->rules());        
+	    $this->form_validation->set_message($this->config->item('msg_error'));
 
-        if ($this->input->server('REQUEST_METHOD') == 'POST'){
-	        $this->form_validation->set_rules($this->user->rules());        
-	        $this->form_validation->set_message($this->config->item('msg_error'));
-
-	        if (!$this->form_validation->run()) {
-	            $data['error_msg'] = array(               
-	                'email' => form_error('email', '<p class="mt-3 text-danger">', '</p>'),
-	                'password' => form_error('password', '<p class="mt-3 text-danger">', '</p>'),                
-	            );
-	            $data['error'] = true;
-	        }else{	        	
-	        	if ($this->user->get_login()->num_rows()>0) {
-	        		$data2 = $this->user->get_login()->row();
-	        		$array = array(
-						'login' => TRUE,
-						'name'=>$data2->name,
-						'email'=>$data2->email,
-						'password'=>$data2->password,
-						'id'=>$data2->id,
-						'user_grup'=>$data2->nama_grup		
-					);					
-					$data['success'] = true;
-					$data['nama'] = $data2->name;
-					$this->session->set_userdata( $array );
-	        	}else{
-	        		$data['wrong']=true;
-	        		$data['wrong_msg'] = '<p class="mt-3 text-danger">Email/Password Salah</p>';
-	        	}
-	        }
-
-	        echo json_encode($data);
+	    if (!$this->form_validation->run()) {
+        $data['error'] = true;
+        $data['error_msg'] = array(               
+          'email' => form_error('email', '<p class="mt-3 text-danger">', '</p>'),
+          'password' => form_error('password', '<p class="mt-3 text-danger">', '</p>'),                
+        );
+	    }else{	        	
+	     	if($this->user->getEmail($this->input->post('email')) == 0){
+           $data['wrong'] = true;
+            $data['wrong_msg'] = '<p class="mt-3 text-danger">Email tidak terdaftar!</p>';
+        }else{
+          if($this->user->get_login()->num_rows()>0) {
+              $data2 = $this->user->get_login()->row();
+              $array = array(
+                'login' => TRUE,
+                'name'=>$data2->name,
+                'email'=>$data2->email,
+                'password'=>$data2->password,
+                'id'=>$data2->id,
+                'user_grup'=>$data2->nama_grup		
+              );					
+              $data['success'] = true;
+              $data['nama'] = $data2->name;
+              $this->session->set_userdata( $array );
+            }else{
+              $data['wrong']=true;
+              $data['wrong_msg'] = '<p class="mt-3 text-danger">Password Salah</p>';
+            }
+          }
+	      }
+	       echo json_encode($data);
 	    }else{
 	    	echo "method not allowed";
 	    }
