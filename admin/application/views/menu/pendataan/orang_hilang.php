1395a24dae7cc34c86d06b0b80c41035d168aeba
Irfan Hakim <irfan_hakim_25rpl@student.smktelkom-mlg.sch.id>
2019-01-05 18:33:09 +0700

    table responsive

--- admin/application/views/menu/pendataan/orang_hilang.php
+++ admin/application/views/menu/pendataan/orang_hilang.php
@@ -1,13 +1,10 @@
-<div class="container-fluid">
   <h4 class="c-grey-900 mT-10 mB-30"><?= $captoranghilang ?></h4>
+  <div class="container-fluid" style="background-color: white;padding: 2%">
   <?php $this->session->set_userdata('redirect',$this->uri->segment(2)); ?>
-  <div class="row">
-    <div class="col-md-12">
-      <div class="bgc-white bd bdrs-3 p-20 mB-20">
         <center>
           <a a href="<?= base_url('pendataan/add')?>"  class="btn cur-p btn-primary">Tambah Data</a>
         </center>
-        <table class="table table-bordered table-striped" id="mytable">
+        <table class="table table-bordered table-striped table-responsive" id="mytable">
           <thead>
             <tr>
               <th>No</th>
@@ -18,10 +15,9 @@
               <th>Jenis Kelamin / Umur</th>
               <th>Alamat</th>
               <th>Aksi</th>
+              <th>asd</th>
+              <th>asd</th>
             </tr>
           </thead>
         </table>
-      </div>
-    </div>
-  </div>
-</div>
+      </div>    
