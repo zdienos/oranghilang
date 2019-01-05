c5e7c027e1a1fecc4816de4add7a4f6223847683
Irfan Hakim <irfan_hakim_25rpl@student.smktelkom-mlg.sch.id>
2019-01-05 21:04:36 +0700

    fix search with space

--- application/models/M_oranghilang.php
+++ application/models/M_oranghilang.php
@@ -62,8 +62,8 @@ class M_oranghilang extends CI_Model {
 
   public function view_search($name){
     $this->load->library('pagination'); // Load librari paginationnya
-    
-    $query = "SELECT orang_hilang.id as id_orang, nama_lengkap, nama_panggilan, umur, tgl_laporan, nama_bencana_alam, nama_status_org,lokasi_terakhir,foto FROM orang_hilang JOIN bencana_alam on bencana_alam.id=orang_hilang.id_bencana_alam JOIN status_org_hilang on status_org_hilang.id=orang_hilang.id_status_org_hilang WHERE nama_lengkap LIKE '%$name%' ORDER BY `tgl_laporan` DESC"  ; // Query untuk menampilkan semua data siswa
+    $nama = str_replace('%20', ' ', $name);
+    $query = "SELECT orang_hilang.id as id_orang, nama_lengkap, nama_panggilan, umur, tgl_laporan, nama_bencana_alam, nama_status_org,lokasi_terakhir,foto FROM orang_hilang JOIN bencana_alam on bencana_alam.id=orang_hilang.id_bencana_alam JOIN status_org_hilang on status_org_hilang.id=orang_hilang.id_status_org_hilang WHERE nama_lengkap LIKE '%$nama%' ORDER BY `tgl_laporan` DESC"  ; // Query untuk menampilkan semua data siswa
     
     $config['base_url'] = base_url('oranghilang/index');
     $config['total_rows'] = $this->db->query($query)->num_rows();
--- application/views/menu/orang_hilang/search_empty.php
+++ application/views/menu/orang_hilang/search_empty.php
@@ -2,7 +2,7 @@
 	<center>
 		<h1><b><u>Daftar Orang Hilang</u></b></h1><br>
 	<section>
-		<div class="container" >
+		<div class="container" style="min-height: 280px">
 			<div class="row">				
 				Pencarian Anda tidak terkait dengan manapun
 			</div>
